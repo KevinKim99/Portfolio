fb58a51c6bb0297fc52c5c33909ca528fda89394
Kevin Kim <rkdgus990503@gmail.com>
2024-09-19 19:16:47 -0700

    implemented landing page for introduction

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,11 +1,15 @@
 import styles from "./App.module.css";
 import { Navbar } from "./components/Navbar/Navbar";
+import { Kevin } from "./components/Kevin/Kevin";
+import { About } from "./components/About/About";
 
 function App() {
 
   return (
     <div className={styles.App}>
       <Navbar />
+      <Kevin />
+      <About />
     </div>
   )
 }

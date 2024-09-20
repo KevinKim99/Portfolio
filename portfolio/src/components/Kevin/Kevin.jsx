6ab1689c23058fad6323035a96f2a0d80d77a996
Kevin Kim <rkdgus990503@gmail.com>
2024-09-19 21:33:14 -0700

    fixed introduction

--- portfolio/src/components/Kevin/Kevin.jsx
+++ portfolio/src/components/Kevin/Kevin.jsx
@@ -7,7 +7,7 @@ export const Kevin = () => {
     <section className={styles.container}>
       <div className={styles.content}>
         <h1 className={styles.title}>Hi, I'm Kevin</h1>
-        <p className={styles.description}>I am 4th year UBC student, interested in pursuing a career as a software engieer with a focus on full-stack or front-end developer. My goal is to make a meaningful impact in the software field!</p>
+        <p className={styles.description}>I am 4th year UBC student majoring Computer Science, interested in pursuing a career as a software engieer with a focus on full-stack or front-end developer. My goal is to make a meaningful impact in the software field!</p>
         <a href="mailto:kanghyun.kevin.kim99@gmail.com" className={styles.contactBtn}>Contact Me!</a>
       </div>
       <img src={getImageUrl("hero/me.png")} alt="Hero image of me" className={styles.heroImg}/>
